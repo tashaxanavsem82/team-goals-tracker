5a1d2b3ade7df3266d7ebbf250134ce267cbaa4f
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 07:28:35 -0500

    feat: initial commit for src/components/GoalList.js

---
+++ src/components/GoalList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoalList = () => {
+  return (
+    <div>
+      <h2>Active Goals</h2>
+      {/* Goals will be displayed here */}
+    </div>
+  );
+};
+
+export default GoalList;
