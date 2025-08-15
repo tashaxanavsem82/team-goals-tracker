0a947862518747815bb7ee2b253792fbe1f81ec7
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 07:28:33 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoalList from './components/GoalList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Goals Tracker</h1>
+      <GoalList />
+    </div>
+  );
+}
+
+export default App;
