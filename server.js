d56fff1ad9c1a0903b446415291341ee7bb6e057
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 07:32:13 -0500

    Add error handling middleware

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
